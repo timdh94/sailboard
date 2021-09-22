0a973f95eeb9f28ca957107b3fd86692671a8fae
timdh94 <timothyh94@gmail.com>
2021-09-22 14:54:40 -0400

    Added some app-wide css. Added routerlink to login form.

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import NavBar from './components/nav/NavBar/NavBar';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import LoginForm from './components/login/Login';
 
 function App() {
   return (
     <Router>
       <div>
         <NavBar />
-        WOWOW
+        <Route path='/login' exact component={LoginForm} />
       </div>
     </Router>
   );
