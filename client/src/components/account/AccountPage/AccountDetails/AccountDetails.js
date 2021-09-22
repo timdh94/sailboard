c785b89d987e9865f39c4d37c5c42d92f7819290
timdh94 <timothyh94@gmail.com>
2021-09-22 18:47:08 -0400

    Created account details component. Loads userinfo from props.

---
+++ client/src/components/account/AccountPage/AccountDetails/AccountDetails.js
@@ -0,0 +1,28 @@
+import './AccountDetails.css';
+
+const AccountDetails = ({ userInfo }) => {
+  return (
+    <div className='user-details-container'>
+      <div className='profile-picture'>
+        profile picture here
+      </div>
+      <div className='user-details'>
+         <div className='user-name'>
+          <p className='details-header'>username:</p>
+          {userInfo.userName}
+        </div>
+        <div className='user-email'>
+          <p className='details-header'>email:</p>
+          {userInfo.email}
+        </div>
+        <div className='user-region'>
+          <p className='details-header'>region:</p>
+          {userInfo.region}
+        </div>
+        <button className='edit-details-button'>edit account</button>
+      </div>
+    </div>
+  )
+};
+
+export default AccountDetails;
