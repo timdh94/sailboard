06f2b4338b51ee3b5af431cf574a3352e82524d6
timdh94 <timothyh94@gmail.com>
2021-09-22 19:36:37 -0400

    Added some conditional render depending on if the user is authenticated. Added basic (bad) logout functionality.

--- client/src/components/nav/NavBar/NavBar.js
+++ client/src/components/nav/NavBar/NavBar.js
@@ -1,7 +1,9 @@
 import './NavBar.css';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-const NavBar = () => {
+// TODO: redirect to home on logout
+const NavBar = ({ isAuthenticated, setUserInfo, setIsAuthenticated }) => {
   return (
     <div className="top-navbar-container">
       <div className="top-navbar"></div>
@@ -10,20 +12,43 @@ const NavBar = () => {
         <Link to={'/browse'} className='top-nav-list'>browse</Link>
         <Link to={'/mycollection'} className='top-nav-list'>my collection</Link>
         <Link to={'/mylistings'} className='top-nav-list'>my listings</Link>
-        <Link 
-          to={'/account'} 
-          className='top-nav-list active top-nav-list-right'>
-          my account
-        </Link>
+        {isAuthenticated ? 
+          <Link 
+            to={'/account'} 
+            className='top-nav-list active top-nav-list-right'>
+            my account
+          </Link>
+        : <></>}
+        {isAuthenticated?
+          <div 
+            onClick={() => {
+              setUserInfo();
+              setIsAuthenticated();
+              localStorage.setItem('accessToken', null);
+            }}
+            className='top-nav-list top-nav-list-right'
+          >logout</div>
+        :
         <Link
-          to={'/login'}
-          className='top-nav-list top-nav-list-right'
-        >
-          login
-        </Link>
+         to={'/login'}
+         className='top-nav-list top-nav-list-right'
+         >login</Link>}
       </ul>
     </div>
   );
 };
 
-export default NavBar;
+const mapStateToProps = (state) => {
+  return {
+    isAuthenticated: state.isAuthenticated
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setUserInfo: () => dispatch({ type: 'SET_INFO', userInfo: null}),
+    setIsAuthenticated: () => dispatch({ type: 'SET_AUTH', status: false })
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavBar);
