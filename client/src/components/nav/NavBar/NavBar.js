55ed8203ae90c5bcfcf5136b788117917cffa3ff
timdh94 <timothyh94@gmail.com>
2021-09-22 13:26:36 -0400

    Created dummy nav bar.

---
+++ client/src/components/nav/NavBar/NavBar.js
@@ -0,0 +1,29 @@
+import './NavBar.css';
+import { Link } from 'react-router-dom';
+
+const NavBar = () => {
+  return (
+    <div className="top-navbar-container">
+      <div className="top-navbar"></div>
+      <ul className="navbar-list">
+        <Link to={'/'} className='top-nav-list active'>home</Link>
+        <Link to={'/browse'} className='top-nav-list'>browse</Link>
+        <Link to={'/mycollection'} className='top-nav-list'>my collection</Link>
+        <Link to={'/mylistings'} className='top-nav-list'>my listings</Link>
+        <Link 
+          to={'/account'} 
+          className='top-nav-list active top-nav-list-right'>
+          my account
+        </Link>
+        <Link
+          to={'/'}
+          className='top-nav-list top-nav-list-right'
+        >
+          logout
+        </Link>
+      </ul>
+    </div>
+  );
+};
+
+export default NavBar;
