51183e42ddeaa990a8a12249320a217df7bb9ddf
timdh94 <timothyh94@gmail.com>
2021-09-22 18:21:20 -0400

    Added account path and component to app.

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import NavBar from './components/nav/NavBar/NavBar';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import LoginForm from './components/login/Login';
 import CreateAccount from './components/account/CreateAccount/CreateAccount';
+import Account from './components/account/AccountPage/Account/Account';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <NavBar />
         <Route path='/login' exact component={LoginForm} />
         <Route path='/createaccount' exact component={CreateAccount} />
+        <Route path='/account' exact component={Account} />
       </div>
     </Router>
   );
