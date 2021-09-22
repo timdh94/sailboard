a5c27d7f109565531acbc590bddc849f0a7d4a13
timdh94 <timothyh94@gmail.com>
2021-09-22 18:23:12 -0400

    Added jwt auth middleware.

---
+++ server/src/middleware/jwtAuth.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+const { ACCESS_TOKEN_SECRET } = require('../config');
+
+const authMiddleware = async (req, res, next) => {
+  const authHeaders = req.headers['authorization'];
+  console.log(authHeaders);
+  if (!authHeaders) res.sendStatus(403);
+  const userJwt = authHeaders.split(' ')[1];
+
+  try {
+    const { uid } = jwt.verify(userJwt, ACCESS_TOKEN_SECRET);
+    if (!uid) res.sendStatus(403);
+    req.userId = uid;
+    next();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+module.exports = authMiddleware;
