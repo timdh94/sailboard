5ef131c8203d6f884849ca040bcb6418e0f4b63e
timdh94 <timothyh94@gmail.com>
2021-09-22 18:23:54 -0400

    Login controller now adds a jwt to the return upon successful auth.

--- server/src/controllers/loginController.js
+++ server/src/controllers/loginController.js
@@ -1,10 +1,11 @@
 const db = require('../models/index');
 const bcrypt = require('bcrypt');
 const { Op } = require('sequelize');
+const jwt = require('jsonwebtoken');
+const { ACCESS_TOKEN_SECRET } = require('../config');
 
 const authenticate = async (req, res) => {
   const loginForm = req.body;
-  console.log(loginForm);
   if (!loginForm.nameOrEmail || !loginForm.password) {
     res.status(400).send('Invalid form info');
     return;
@@ -22,9 +23,28 @@ const authenticate = async (req, res) => {
     }
   });
   
-  if (!user) {
+  if (!user || !user.Password) {
     res.status(400).send({ message: 'Invalid username or password' });
   }
+  
+  await bcrypt.compare(loginForm.password, user.Password.dataValues.password)
+    .then(async (success) => {
+      if (!success) {
+        res.status(300).send({
+          message: 'Invalid username or password'
+        });
+        return;
+      }
+      const token = jwt.sign({ uid: user.id }, ACCESS_TOKEN_SECRET, {
+        expiresIn: '7d'
+      });
+      
+      res.status(200).send({
+        message: 'Logged in',
+        accessToken: token,
+      });
+      // ADD JWT SIGN LOGIC HERE
+    });
 };
 
 
