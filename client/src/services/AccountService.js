f7222c61d6f4be577562f209c88509642c40b92a
timdh94 <timothyh94@gmail.com>
2021-09-22 18:21:57 -0400

    Changed object layout of account service. Added getting user info functionality.

--- client/src/services/AccountService.js
+++ client/src/services/AccountService.js
@@ -1,19 +1,32 @@
 const url = 'http://localhost:3005/users';
 
-const AccountService = {
-  createAccount: async (accountForm) => {
-    const res = await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      crendentials: 'include',
-      body: JSON.stringify(accountForm)
-    })
-      .then(res => res.json())
-      .catch(err => console.log(err));
-    return res;
-  }
+const AccountService = {};
+
+AccountService.createAccount = async (accountForm) => {
+  return fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    crendentials: 'include',
+    body: JSON.stringify(accountForm)
+  })
+    .then(res => res.json())
+    .catch(err => console.log(err));
+};
+  
+AccountService.getUserInfo = async (accessToken) => {
+  return fetch(url, {
+    method: 'GET',
+    credentials: 'include',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${accessToken}`
+    },
+  })
+    .then(res => res.json())
+    .catch(err => console.log(err));
 };
 
 export default AccountService; 
