b893a4347bd4f2846ab99d8dc7c43e7a7dd777a1
timdh94 <timothyh94@gmail.com>
2021-09-22 18:23:19 -0400

    Added get user info router that requires auth middleware.

--- server/src/router.js
+++ server/src/router.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
 const LoginController = require('./controllers/loginController');
 const userController = require('./controllers/userController');
+const authMiddleware = require('./middleware/jwtAuth');
 
 router.post('/login', LoginController.authenticate);
 router.post('/users', userController.createAccount);
+// TODO: AUTH MIDDLEWARE HERE
+router.get('/users/', authMiddleware, userController.getUserById);
 
 module.exports = router;
