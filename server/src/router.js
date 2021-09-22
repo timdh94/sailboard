1595978f3768a184ed6f12d8a736c1fb4ec4b1f5
timdh94 <timothyh94@gmail.com>
2021-09-22 16:43:58 -0400

    Added create account route.

--- server/src/router.js
+++ server/src/router.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const LoginController = require('./controllers/loginController');
+const userController = require('./controllers/userController');
 
 router.post('/login', LoginController.authenticate);
+router.post('/users', userController.createAccount);
 
 module.exports = router;
