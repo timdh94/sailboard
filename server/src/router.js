12258f8b9ca4d4e3c11897529a8fce9bdbc1acd1
timdh94 <timothyh94@gmail.com>
2021-09-23 11:34:28 -0400

    Added collection routes

--- server/src/router.js
+++ server/src/router.js
@@ -1,11 +1,15 @@
 const router = require('express').Router();
 const LoginController = require('./controllers/loginController');
 const userController = require('./controllers/userController');
+const collectionController = require('./controllers/collectionController');
 const authMiddleware = require('./middleware/jwtAuth');
 
 router.post('/login', LoginController.authenticate);
+
 router.post('/users', userController.createAccount);
-// TODO: AUTH MIDDLEWARE HERE
-router.get('/users/', authMiddleware, userController.getUserById);
+router.get('/users', authMiddleware, userController.getUserById);
+
+router.get('/collection', authMiddleware, collectionController.getUserCollection);
+router.post('/collection', authMiddleware, collectionController.addKeyboardToCollection);
 
 module.exports = router;
