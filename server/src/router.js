70f551cdd2edc03443d37d88567956080037f31d
timdh94 <timothyh94@gmail.com>
2021-09-23 13:46:30 -0400

    Added listing routes.

--- server/src/router.js
+++ server/src/router.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const LoginController = require('./controllers/loginController');
 const userController = require('./controllers/userController');
 const collectionController = require('./controllers/collectionController');
+const listingController = require('./controllers/listingController');
 const authMiddleware = require('./middleware/jwtAuth');
 
 router.post('/login', LoginController.authenticate);
@@ -11,5 +12,8 @@ router.get('/users', authMiddleware, userController.getUserById);
 
 router.get('/collection', authMiddleware, collectionController.getUserCollection);
 router.post('/collection', authMiddleware, collectionController.addKeyboardToCollection);
+router.delete('/collection/:id', authMiddleware, collectionController.deleteKeyboard);
+
+router.post('/listings', authMiddleware, listingController.createListing);
 
 module.exports = router;
