b2a448ea8c4f653b93f9a193905560455e905326
timdh94 <timothyh94@gmail.com>
2021-09-23 21:08:08 -0400

    Added in progress logout functionality. Added conditional display for user-profile menu options.

--- client/src/components/nav/NavBar/NavBar.js
+++ client/src/components/nav/NavBar/NavBar.js
@@ -1,28 +1,42 @@
 import './NavBar.css';
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 // TODO: redirect to home on logout
 const NavBar = () => {
   const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+  const dispatch = useDispatch();
+  const history = useHistory();
+  
+  // need to do more here
+  const logout = () => {
+    localStorage.removeItem('accessToken');
+    dispatch({ type: 'LOGOUT' })
+    dispatch({ type: 'SET_BOARDS', payload: [] });
+    dispatch({ type: 'SET_USER_LISTINGS', payload: [] });
+    history.push('/');
+  };
+
   return (
     <div className="top-navbar-container">
       <div className="top-navbar"></div>
       <ul className="navbar-list">
         <Link to={'/'} className='top-nav-list active'>home</Link>
         <Link to={'/browse'} className='top-nav-list'>browse</Link>
-        <Link to={'/mycollection'} className='top-nav-list'>my collection</Link>
-        <Link to={'/mylistings'} className='top-nav-list'>my listings</Link>
-        {isAuthenticated ? 
+        {isAuthenticated && <Link to={'/mycollection'} className='top-nav-list'>my collection</Link>}
+        {isAuthenticated && <Link to={'/mylistings'} className='top-nav-list'>my listings</Link>}
+        {isAuthenticated &&
           <Link 
             to={'/account'} 
             className='top-nav-list active top-nav-list-right'>
             my account
           </Link>
-        : <></>}
-        {isAuthenticated?
+        }
+        {isAuthenticated ?
           <div 
             className='top-nav-list top-nav-list-right'
+            onClick={logout}
           >logout</div>
         :
         <Link
