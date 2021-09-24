02a5435dfe022017cfc526bd18238edd04c36be8
timdh94 <timothyh94@gmail.com>
2021-09-24 16:27:11 -0400

    Now checks for new bids on each listing on page load. Displays a message if there are new bids. (need to improve this significantly)

--- client/src/components/userListings/UserListings/UserListings.js
+++ client/src/components/userListings/UserListings/UserListings.js
@@ -4,13 +4,19 @@ import { useHistory } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import ListingService from '../../../services/listingService';
 import KeyboardListing from '../../browse/KeyboardListing/KeyboardListing';
+import BidService from '../../../services/BidService';
 
 const UserListings = () => {
   const history = useHistory();
   const dispatch = useDispatch();
   const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
-  const userListings = useSelector(state => (state.listings));
+  const [userListings, setUserListings] = useState([]);
   const [isLoading, setIsLoading] = useState(true)
+  
+  const getListingBids = async (listingId) => {
+    const res = await BidService.getListingBids(listingId);
+    return res;
+  };
   
   useEffect(() => {
     if (!isAuthenticated) {
@@ -21,12 +27,20 @@ const UserListings = () => {
       const accessToken = localStorage.getItem('accessToken');
       const res = await ListingService.getUserListings(accessToken);
       if (res.userListings) {
-        dispatch({
+        for (const listing of res.userListings) {
+          const res = await BidService.getListingBids(listing.id);
+          if (res.bids.length > 0) {
+            listing.newBids = true;
+          }
+        }
+        setUserListings(res.userListings);
+      }
+      dispatch({
           type: 'SET_USER_LISTINGS',
           payload: res.userListings
-        });
-      }
+      });
     })();
+    
     setIsLoading(false);
   }, [dispatch, history, isAuthenticated]);
   
@@ -39,6 +53,7 @@ const UserListings = () => {
           <KeyboardListing
           listing={listing}
           />
+          {listing.newBids && <div>New bids!! Respond ASAP!</div>}
           <input
             className='unlist-button'
             type='button'
