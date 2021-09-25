65e16a2479b20fdf72b976dce9c14e087198b6a7
timdh94 <timothyh94@gmail.com>
2021-09-25 09:57:35 -0400

    Now renders all bids for a listing on the userlistings page.

--- client/src/components/userListings/UserListings/UserListings.js
+++ client/src/components/userListings/UserListings/UserListings.js
@@ -4,19 +4,14 @@ import { useHistory } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import ListingService from '../../../services/listingService';
 import KeyboardListing from '../../browse/KeyboardListing/KeyboardListing';
-import BidService from '../../../services/BidService';
+import BidResponse from '../../bids/BidResponse/BidResponse';
 
 const UserListings = () => {
   const history = useHistory();
   const dispatch = useDispatch();
   const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
-  const [userListings, setUserListings] = useState([]);
+  const userListings = useSelector(state => state.listings);
   const [isLoading, setIsLoading] = useState(true)
-  
-  const getListingBids = async (listingId) => {
-    const res = await BidService.getListingBids(listingId);
-    return res;
-  };
   
   useEffect(() => {
     if (!isAuthenticated) {
@@ -26,34 +21,25 @@ const UserListings = () => {
     (async () => {
       const accessToken = localStorage.getItem('accessToken');
       const res = await ListingService.getUserListings(accessToken);
-      if (res.userListings) {
-        for (const listing of res.userListings) {
-          const res = await BidService.getListingBids(listing.id);
-          if (res.bids.length > 0) {
-            listing.newBids = true;
-          }
-        }
-        setUserListings(res.userListings);
-      }
-      dispatch({
-          type: 'SET_USER_LISTINGS',
-          payload: res.userListings
+      // TODO: BID STUFF HERE
+      if (res.userListings) dispatch({
+        type: 'SET_USER_LISTINGS',
+        payload: res.userListings
       });
     })();
-    
     setIsLoading(false);
   }, [dispatch, history, isAuthenticated]);
   
   
-  if (isLoading) return (<></>);
+  if (isLoading || !userListings) return (<></>);
   return (
     <div>
       {userListings.map(listing => (
         <div key={listing.id}>
-          <KeyboardListing
-          listing={listing}
-          />
-          {listing.newBids && <div>New bids!! Respond ASAP!</div>}
+          <KeyboardListing listing={listing} />
+          {listing.Bids.map(bid => (
+           <BidResponse bid={bid} key={bid.id}/> 
+          ))}
           <input
             className='unlist-button'
             type='button'
